168b18c2ed97bc621088fa5d0b6a1ba2304738d3
Doujins <15114090298@163.com>
2016-07-03 14:44:12 +0800

    Two

---
+++ 习题/ex6.py
@@ -0,0 +1,12 @@
+x = "oh? %d ...  " % 233
+
+y = "no zuo no die %s %s " % ('why you','try?')
+
+print x,y
+
+z1 = "I am "
+z2 = "sorry"
+
+print z1+z2
+
+print "%r" % True
---
+++ 习题/ex8.py
@@ -0,0 +1,10 @@
+formatter = "%r %r %r"
+
+print formatter % (1,2,3)
+print formatter % ('one','two','three')
+print formatter % (True,False,True)
+print formatter % (
+	"I",
+	"Am",
+	"Doujins"
+)
