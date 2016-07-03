067af496f341b23a738bbcd9442a80908bb736b0
Doujins <15114090298@163.com>
2016-07-03 14:18:42 +0800

    Tets

---
+++ 习题/ex4.py
@@ -0,0 +1,13 @@
+cars = 100
+space_in_a_car = 4.0
+#carpool_capacity = cars_driven * space_in_a_car
+
+print "There are", cars, "cars available."
+#print "We can transport", carpool_capacity, "people today."
+
+'''
+Traceback (most recent call last):
+  File "ex4.py", line 3, in <module>
+    carpool_capacity = cars_driven * space_in_a_car
+NameError: name 'cars_driven' is not defined
+'''
---
+++ 习题/ext3.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+print 1.0/2
+print 1/3
+print 1//3
+print 1.0//3
